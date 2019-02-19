96f4b10a675a0dca1a12867cd78bead555d4b208
bidbear <1140136143@qq.com>
2019-02-19 15:45:06 +0800

    首页链接配置

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from wx import views
 urlpatterns = [
+    path('',views.index),
     path('admin/', admin.site.urls),
-    path('wx',views.index),
+    path('',views.wx),
 ]
