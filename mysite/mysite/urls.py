ed58137840c2e71ae2c98554afcd790cfa56754b
bidbear <1140136143@qq.com>
2019-02-19 15:35:15 +0800

    测试连接

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from wx import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('wx',views.index),
 ]
