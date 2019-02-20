c68a9ffb2bf4df7b80a1972a35cd0d2eb6ae69e8
bidbear <1140136143@qq.com>
2019-02-20 11:16:52 +0800

    测试获取access——token

--- mysite/wx/views.py
+++ mysite/wx/views.py
@@ -15,8 +15,8 @@ def index(request):
     url="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx32ed607e6951016c&secret=e65acf0f687135c8f953f26f52cdb2d0"
     all_url=url+url_parame
     data=urllib.request.urlopen(all_url).read()
-    record=data.decode('UTF-8')
-    print(record.access_token)
+    record=json.dumps(data.decode('UTF-8'))
+    print(record['access_token'])
     return render(request,'index.html')
 
 #django默认开启csrf防护，这里使用@csrf_exempt去掉防护    
