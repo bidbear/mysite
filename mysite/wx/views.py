450af026d0fce51fbb058f1c05a68f608f3ded17
bidbear <1140136143@qq.com>
2019-02-21 17:12:27 +0800

    处理图片消息

--- mysite/wx/views.py
+++ mysite/wx/views.py
@@ -38,37 +38,30 @@ def wx(request):
 
 # post请求处理
 def autoreply(request):
-    # 获取token
-    data = Wx_Access_Token.objects.all().last()
-    access_token = data.access_token
     try:
         webData = request.body
         xmlData = ET.fromstring(webData)
+
         msg_type = xmlData.find('MsgType').text
         ToUserName = xmlData.find('ToUserName').text
         FromUserName = xmlData.find('FromUserName').text
         CreateTime = xmlData.find('CreateTime').text
         MsgType = xmlData.find('MsgType').text
         MsgId = xmlData.find('MsgId').text
-        Content = xmlData.find('Content').text
-       
 
         toUser = FromUserName
         fromUser = ToUserName
-  
-        if msg_type == 'image':
-            content = "图片已收到,谢谢"
+
+        if msg_type == 'text':
+            content = "您好,欢迎来到Python大学习!希望我们可以一起进步!"
             replyMsg = TextMsg(toUser, fromUser, content)
+            print "成功了!!!!!!!!!!!!!!!!!!!"
+            print replyMsg
             return replyMsg.send()
 
-        elif msg_type == 'text': 
-            if Content not in (1,2):
-                content = "你要接受心理测试么？\n 1.是 \n 2.否"
-                if Content == '1':
-                    content = '好'
-                if Content == '2':
-                    content ='不好'
-                replyMsg = TextMsg(toUser, fromUser, content)
+        elif msg_type == 'image':
+            content = "图片已收到,谢谢"
+            replyMsg = TextMsg(toUser, fromUser, content)
             return replyMsg.send()
         elif msg_type == 'voice':
             content = "语音已收到,谢谢"
@@ -91,10 +84,10 @@ def autoreply(request):
             content = "链接已收到,谢谢"
             replyMsg = TextMsg(toUser, fromUser, content)
             return replyMsg.send()
-    except Exception as e:
-        return 404
 
-# 被动回复消息
+    except Exception, Argment:
+        return Argment
+
 class Msg(object):
     def __init__(self, xmlData):
         self.ToUserName = xmlData.find('ToUserName').text
@@ -123,6 +116,10 @@ class TextMsg(Msg):
         </xml>
         """
         return XmlForm.format(**self.__dict)
-    
+--------------------- 
+作者：Peace & Love 
+来源：CSDN 
+原文：https://blog.csdn.net/u013205877/article/details/77602853 
+版权声明：本文为博主原创文章，转载请附上博文链接！
 
 
