665c86e8b5320704175c120133dbe1ba92d4a222
bidbear <1140136143@qq.com>
2019-02-21 14:47:31 +0800

    处理文本消息

--- mysite/wx/util.py
+++ mysite/wx/util.py
@@ -1,18 +1,17 @@
 
-class dealtext(FromUserName,access_token):
+class DealText(toUser,access_token):
     """docstring for """
-    def __init__(self, FromUserName,access_token):
-        self.FromUserName = FromUserName
+    def __init__(self, toUser,access_token):
+        self.toUser = toUser
         self.access_token = access_token
 
     def getrequest(self):
         data={}
         url_parame=urllib.parse.urlencode(data)
-        url="https://api.weixin.qq.com/cgi-bin/user/info?access_token="+self.access_token+"&openid="+self.FromUserName+"&lang=zh_CN"
+        url="https://api.weixin.qq.com/cgi-bin/user/info?access_token="+self.access_token+"&openid="+self.toUser+"&lang=zh_CN"
         all_url=url+url_parame
         data=urllib.request.urlopen(all_url).read()
         print(data)
         print('-------------------------------')
         record=json.loads(data.decode('UTF-8')) 
         print(record)
-        
