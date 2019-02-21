e5621350add10c9a83c38c2f5903ebf3d0cea91a
bidbear <1140136143@qq.com>
2019-02-21 11:11:47 +0800

    查看token

--- mysite/wx/models.py
+++ mysite/wx/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Wx_Access_Token(models.Model):
     """docstring for Wx_Access_Token"""
     access_token = models.CharField(max_length=1024)
-    once_time = models.DateTimeField(auto_now=True)
+    created_time = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.access_token
