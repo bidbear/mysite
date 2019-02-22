f1a1434c0e534d273f539fbff655461ada42f0ee
bidbear <1140136143@qq.com>
2019-02-22 16:16:36 +0800

    pic to word

--- mysite/wx/views.py
+++ mysite/wx/views.py
@@ -72,7 +72,7 @@ def autoreply(request):
             if CONTENT == 1:
                 content = Toword(PicUrl)
             if CONTENT == 2:
-                content = SaveImg(PicUrl)
+                content = SaveImg(PicUrl,getidCard)
             replyMsg = TextMsg(toUser, fromUser, content)
             return replyMsg.send()
 
