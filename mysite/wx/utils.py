8d0f06b33e6908567b47addfbe54a4b5e0299ac8
bidbear <1140136143@qq.com>
2019-02-22 17:21:40 +0800

    修改关键字回复内容

--- mysite/wx/utils.py
+++ mysite/wx/utils.py
@@ -65,5 +65,5 @@ def SaveImg(picurl):
     response = requests.get(img_src)
     image = Image.open(BytesIO(response.content))
     image.save('/data/wwwroot/mysite/mysite/static/idCard/123.jpg')
-    print('保存成功%s' % func)
+    print('保存成功')
     return getidCard()
