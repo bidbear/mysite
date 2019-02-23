ce6f8bcf9e20309df7e13b731274d1e4beac3192
bidbear <1140136143@qq.com>
2019-02-23 13:44:08 +0800

    关注自动回复

--- mysite/wx/views.py
+++ mysite/wx/views.py
@@ -56,7 +56,8 @@ def autoreply(request):
         fromUser = ToUserName
 
         events = xmlData.find('Event').text
-        
+        print(events)
+        print('#######################################')
         if events =='subscribe':
             content = "感谢订阅，目前公众号具有图片识别的功能，请不要频繁使用。。。。"
             replyMsg = TextMsg(toUser, fromUser, content)
